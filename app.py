8dd01bae87185549be5a2ef4aa5b4d9315c843b2
Sentains <iliyasemenov2021@gmail.com>
2026-03-31 16:52:48 +0400

    Update app.py

--- app.py
+++ app.py
@@ -11,5 +11,27 @@ def training_of_fly(prof):
     params = {'prof': prof, 'title': 'Тренинг'}
     return render_template('training_of_fly.html', **params)
 
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    professions = ['инженер-исследователь',
+                   'пилот',
+                   'строитель',
+                   'экзобиолог',
+                   'врач',
+                   'инженер по терраформированию',
+                   'климатолог',
+                   'специалист по радиационной защите',
+                   'астрогеолог',
+                   'гляциолог',
+                   'инженер жизнеобеспечения',
+                   'метеоролог',
+                   'оператор марсохода',
+                   'киберинженер',
+                   'штурман',
+                   'пилот дронов',
+                   ]
+    return render_template('list_of_professions.html', professions=professions, type_list=list)
+
+
 if __name__ == "__main__":
     app.run(port=8080, host='127.0.0.1')
